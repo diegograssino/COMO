4f47212d96a4c3dfd12c87cdb2232adea665de72
Ezequiel Ramirez <ezequielram@gmail.com>
2021-11-02 23:53:50 -0300

    add about section

---
+++ src/components/About.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import { Row, Col, Container, Image, Carousel } from 'react-bootstrap';
+import imagen from '../assets/img/about1.jpg'
+import imagen2 from '../assets/img/about2.jpg'
+import imagen3 from '../assets/img/about3.jpg'
+import imagen4 from '../assets/img/about4.jpg'
+import '../css/index.css'
+
+const About = () => {
+    return (
+        <Container>
+            <Row>
+                <Col md={12} className=" text-center aboutContainer">
+                    <h1 className="tituloAbout">Nosotros</h1>
+                    <p className="subtituloAbout">Somos Ceci y Pachu. Amigos hace años. Un día nos unimos para crear este hermoso proyecto.</p>
+                    <Container className="imagenesAbout">
+                        <Row>
+                            <Col md={7}><Image className="containerFoto pb-3" src={imagen} /></Col>
+                            <Col xs={5}><Image className="containerFoto pb-0" src={imagen2} /></Col>
+                        </Row>
+                        <Row>
+                            <Col md={5}><Image className="containerFoto" src={imagen3} /></Col>
+                            <Col md={7}><Image className="containerFoto" src={imagen4} /></Col>
+                        </Row>
+                    </Container>
+
+                    <Carousel className="carrusellMovil">
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src={imagen}
+                                alt="First slide"
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src={imagen2}
+                                alt="Second slide"
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src={imagen3}
+                                alt="Third slide"
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src={imagen4}
+                                alt="Third slide"
+                            />
+                        </Carousel.Item>
+                    </Carousel>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default About
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import About from './About'
 import Banner from './Banner'
 
 const Home = () => {
     return (
         <>
         <Banner/>
-            <h1>home</h1>
+        <About/>
         </>
     )
 }
